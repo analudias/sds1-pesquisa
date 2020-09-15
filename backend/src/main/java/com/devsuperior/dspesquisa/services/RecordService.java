e6ce608543132268edc19f2a357c0e2d1ec5749c
Ana Luisa Dias <analudias@live.com>
2020-09-15 14:49:30 -0300

    POST records

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.Record;
+import com.devsuperior.dspesquisa.repositories.GameRepository;
+import com.devsuperior.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+	
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		//getOne instanceia um objeto do tipo relacionado necessário, mas não vai no banco de dados ainda, somente para ser associado.
+		//Quando for necessário salvar o record, ele vai até o banco de dados e salva o record com o id que foi passado.
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+	}
+
+}
