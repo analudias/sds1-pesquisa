dbd91317789fe927eb082ce93c33f398c3b69fe6
Ana Luisa Dias <analudias@live.com>
2020-09-16 23:15:59 -0300

    Criando navegação com rotas

---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'; /*componente importado do react-router-dom*/
+import Header from './components/Header';
+import Home from './pages/Home';
+import Records from './pages/Records';
+
+const Routes = () => (
+    <BrowserRouter>
+    <Header /*para inserir o título em todas as páginas *//>
+        <Switch /*decide qual rota será carregada*/> 
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/records" /*para sermos direcionado para essa página ao clicarmos no botão da tela inicial*/>
+                <Records />
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
